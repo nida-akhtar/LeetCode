fde19a9c885ddef56fad0fba86cfdd78f88147d0
Nida Pervaiz <124860931+nida-akhtar@users.noreply.github.com>
2024-10-16 15:01:46 +0500

    Time: 326 ms (90.12%) | Memory: 170.1 MB (67.10%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -13,25 +13,27 @@ public:
             return -1;
         }
         
-        // Move the accessed key-value pair to the front (most recently used)
-        moveToFront(key);
+        // If the key is found, move the accessed element to the front (most recently used)
+        cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
         
-        // Return the value associated with the key
+        // Return the value of the key
         return cacheMap[key]->second;
     }
     
     // Put the key-value pair in the cache. If the number of keys exceeds the capacity, evict the least recently used key.
     void put(int key, int value) {
-        // If the key already exists, update the value and move it to the front
+        // If the key already exists, we update the value and move the key to the front (most recently used)
         if (cacheMap.find(key) != cacheMap.end()) {
-            moveToFront(key);
+            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
             cacheMap[key]->second = value;  // Update the value
             return;
         }
         
-        // If the cache is full, remove the least recently used element (from the back)
+        // If the cache is full, remove the least recently used element (at the back of the list)
         if (cacheList.size() == capacity) {
-            evictLRU();
+            int leastUsedKey = cacheList.back().first;
+            cacheList.pop_back();
+            cacheMap.erase(leastUsedKey);
         }
         
         // Insert the new key-value pair at the front (most recently used position)
@@ -40,28 +42,12 @@ public:
     }
 
 private:
-    // Cache capacity
+    // Define the capacity of the LRU cache
     int capacity;
     
-    // List of key-value pairs, where the front is the most recently used and the back is the least recently used
+    // A list of key-value pairs where the most recently used element is at the front, and the least recently used is at the back
     list<pair<int, int>> cacheList;
     
-    // Unordered map for fast access to list nodes by key
+    // An unordered map for fast access to the list nodes by key
     unordered_map<int, list<pair<int, int>>::iterator> cacheMap;
-
-    // Move the given key to the front of the list (most recently used)
-    void moveToFront(int key) {
-        // If the key is already at the front, no need to move it
-        if (cacheMap[key] != cacheList.begin()) {
-            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
-        }
-    }
-    
-    // Evict the least recently used element from the cache
-    void evictLRU() {
-        // Get the key of the least recently used element (back of the list)
-        int leastUsedKey = cacheList.back().first;
-        cacheList.pop_back();             // Remove the element from the list
-        cacheMap.erase(leastUsedKey);     // Erase it from the map
-    }
 };
