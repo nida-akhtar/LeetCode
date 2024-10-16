7c209b699c04bf19c4781a74564ed98db0735684
Nida Pervaiz <124860931+nida-akhtar@users.noreply.github.com>
2024-10-16 15:00:10 +0500

    Time: 327 ms (89.22%), Space: 170.1 MB (78.46%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,81 +1,53 @@
-class Node{
-    public:
-    int key;
-    int val;
-    Node*next;
-    Node*prev;
-    Node(int key,int val){
-        this->key=key;
-        this->val=val;
-        next=NULL;
-        prev=NULL;
-    }
-};
+#include <unordered_map>
+#include <list>
+using namespace std;
+
 class LRUCache {
 public:
-unordered_map<int,Node*>m;
-    Node*head=new Node(-1,-1);
-    Node*tail=new Node(-1,-1);
-    int size;
-    LRUCache(int capacity)
-     {
-           size=capacity;
-            head->next=tail;
-             tail->prev=head;
-    }   
-    void deleteNode(Node*p){
-       Node*pre=p->prev;
-       Node*nex=p->next;
-       pre->next=nex;
-       nex->prev=pre;
-    }
-    void addNode(Node*newnode){
-        Node*temp=head->next;
-        head->next=newnode;
-        newnode->prev=head;
-        newnode->next=temp;
-        temp->prev=newnode;
-    }
+    LRUCache(int capacity) : capacity(capacity) {}
     
+    // Get the value of the key if the key exists in the cache, otherwise return -1.
     int get(int key) {
-       if(m.find(key)==m.end())
-        return -1;
-        Node*p=m[key];
-        deleteNode(p);
-        addNode(p);
-        m[key]=head->next;
-        return head->next->val;
+        // If the key is not found, return -1
+        if (cacheMap.find(key) == cacheMap.end()) {
+            return -1;
+        }
+        
+        // If the key is found, move the accessed element to the front (most recently used)
+        cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+        
+        // Return the value of the key
+        return cacheMap[key]->second;
     }
     
+    // Put the key-value pair in the cache. If the number of keys exceeds the capacity, evict the least recently used key.
     void put(int key, int value) {
-      if(m.find(key)!=m.end()){
-            Node*c=m[key];
-            deleteNode(c);
-            c->val=value;
-            addNode(c);
-            m[key]=head->next;
+        // If the key already exists, we update the value and move the key to the front (most recently used)
+        if (cacheMap.find(key) != cacheMap.end()) {
+            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+            cacheMap[key]->second = value;  // Update the value
+            return;
         }
-        else{
-            if(m.size()==size){
-                Node* prev=tail->prev;
-                deleteNode(prev);
-                Node*l=new Node(key,value);
-                addNode(l);
-                m.erase(prev->key);
-                m[key]=head->next;
-            }
-            else{
-                 Node*l=new Node(key,value);
-                addNode(l);
-                m[key]=head->next; 
-            }
+        
+        // If the cache is full, remove the least recently used element (at the back of the list)
+        if (cacheList.size() == capacity) {
+            int leastUsedKey = cacheList.back().first;
+            cacheList.pop_back();
+            cacheMap.erase(leastUsedKey);
         }
+        
+        // Insert the new key-value pair at the front (most recently used position)
+        cacheList.emplace_front(key, value);
+        cacheMap[key] = cacheList.begin();
     }
-};
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
+private:
+    // Define the capacity of the LRU cache
+    int capacity;
+    
+    // A list of key-value pairs where the most recently used element is at the front, and the least recently used is at the back
+    list<pair<int, int>> cacheList;
+    
+    // An unordered map for fast access to the list nodes by key
+    unordered_map<int, list<pair<int, int>>::iterator> cacheMap;
+};
